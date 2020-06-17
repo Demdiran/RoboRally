5bcad3c053aaf608f3eb930da3b09442d83c3f15
Eelco Timmerman <eelco.timmerman@live.nl>
2020-06-17 14:23:39 +0200

    next try

--- roborallyAPI/src/main/java/nl/sogyo/roborally/api/RoborallyWebsocket.java
+++ roborallyAPI/src/main/java/nl/sogyo/roborally/api/RoborallyWebsocket.java
@@ -19,11 +19,7 @@ import nl.sogyo.roborally.domain.squares.BoardFactory;
 
 @ServerEndpoint(value = "/websocket")
 public class RoborallyWebsocket{
-<<<<<<< HEAD
     private static final Roborally roborally = new Roborally(BoardFactory.createTESTBOARD4X4());
-=======
-    private static final Roborally roborally = new Roborally(BoardFactory.createFastConveyorBeltFrontEndGameBoard());
->>>>>>> master
     private static final List<Session> players = new ArrayList<>();
     private static final Map<Session, Robot> robots = new HashMap<>();
     
